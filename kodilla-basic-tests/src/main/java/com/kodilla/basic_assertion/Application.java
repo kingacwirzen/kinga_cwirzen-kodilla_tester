5f4da030e2a32dc7b080ca7e2dc4ec99f1092213
Kinga Ćwirzeń <kinga.cwirzen@icloud.com>
2020-08-03 20:35:44 +0200

    3.5

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -13,12 +13,21 @@ public class Application {
             System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
         }
         int subtractResult = calculator.subtract(a, b);
-        boolean right = ResultChecker.assertEquals(-3, subtractResult);
-        if (right) {
+        boolean correctSub = ResultChecker.assertEquals(-3, subtractResult);
+        if (correctSub) {
             System.out.println("Metoda subtract działa poprawnie dla liczb " + a + " i " + b);
         } else {
             System.out.println("Metoda subtract nie działa poprawnie dla liczb " + a + " i " + b);
         }
+        int involutionResult = calculator.involution(a);
+        boolean correctInv = ResultChecker.assertEquals(25, involutionResult);
+        if (correctInv) {
+            System.out.println("Metoda involution działa poprawnie dla liczb " + a);
+        } else {
+            System.out.println("Metoda involution nie działa poprawnie dla liczby " + a);
+        }
+        }
+
     }
-}
+
 
--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -8,4 +8,8 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
+
+    public int involution(int a) {
+        return a * a;
+    }
 }
