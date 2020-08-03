a16d68f857e2d5ce1faaba19d187ab77b4cc85d0
Kinga Ćwirzeń <kinga.cwirzen@icloud.com>
2020-08-03 18:12:40 +0200

    commit

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,14 @@
+public class Application {
+    private static int max;
+    private static int min;
+
+    public int getMax() {
+        return max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+}
+
